bcdd76b111038a9fa1879558d02b1ec6ff68da03
kirillpak <kirill.pak@gmail.com>
2018-12-04 15:12:46 -0500

    added projects page

--- server.js
+++ server.js
@@ -47,6 +47,12 @@ app.get('/about', (req, res) => {
 	})
 })
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		header: 'Your portfolio here:',
+	})
+})
+
 app.get('/bad', (req, res) => {
 	res.send({
 		errorMessage: 'Unable to handle request'
